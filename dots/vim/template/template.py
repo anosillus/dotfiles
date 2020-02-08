7e705dd84270c7a0013d0ebd19036f7e28be8e06
anosillus <anosillus@gmail.com>
2020-02-08 19:44:26 +0900

    fix template

--- dots/vim/template/template.py
+++ dots/vim/template/template.py
@@ -3,18 +3,14 @@
 # vim:fenc=utf-8
 # File name: <+FILE NAME+>
 # First Edit: <+DATE+>
-# Last Change: 19-Jan-2020.
+# Last Change: 08-Feb-2020.
 """
 This scrip is for test
 <+CURSOR+>
 """
-port sys
-
-import pandas as pd
 
 
 def main():
-    pd.read_csv("test")
     pass
 
 
