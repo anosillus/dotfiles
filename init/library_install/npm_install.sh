c89ff1a5daef9548afb3256cd8ede0a5978672b8
anosillus <anosillus@gmail.com>
2020-10-07 15:11:13 +0900

    textlint

--- init/library_install/npm_install.sh
+++ init/library_install/npm_install.sh
@@ -1,6 +1,1 @@
-npm install -g dockerfile_lint
-npm install -g write-good
-npm install -g markdownlint
-npm install -g alex
-npm install -g prettier --save-dev --save-exact
-npm install -g zenn-cli
+npm install -g bash-language-server eslint htmlhint mdl proselint prettier textlint write-good  @textlint-ja/textlint-rule-no-insert-dropping-sa @textlint-rule/textlint-rule-preset-google dictionary-en textlint-filter-rule-comments textlint-rule-apostrophe textlint-rule-common-misspellings textlint-rule-date-weekday-mismatch textlint-rule-diacritics textlint-rule-en-capitalization textlint-rule-en-max-word-count textlint-rule-footnote-order textlint-rule-general-novel-style-ja textlint-rule-ginger textlint-rule-ja-hiragana-fukushi textlint-rule-ja-hiragana-hojodoushi textlint-rule-max-number-of-lines textlint-rule-no-hoso-kinshi-yogo textlint-rule-no-start-duplicated-conjunction textlint-rule-period-in-list-item textlint-rule-prefer-tari-tari textlint-rule-preset-foreign-language-writing textlint-rule-preset-ja-technical-writing textlint-rule-preset-jtf-style textlint-rule-spellcheck-tech-word textlint-rule-spelling textlint-rule-stop-words textlint-rule-terminology textlint-rule-web-plus-db
