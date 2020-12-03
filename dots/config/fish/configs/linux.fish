31c97af08e43a9b7e31830edc263314148ff6cab
anosillus <anosillus@gmail.com>
2020-12-03 12:28:44 +0900

    goneovim fix

--- dots/config/fish/configs/linux.fish
+++ dots/config/fish/configs/linux.fish
@@ -8,6 +8,5 @@ set -x -U GOPATH $HOME/go
 # set -U fish_user_paths  $fish_user_paths
 set PATH $PATH /usr/local/bin /usr/sbin $GOPATH/bin $HOME/.local/bin $HOME/.cargo/env $HOME/.cargo/bin
 
-
 # set -g -x CPPFLAGS "$CPPFLAGS -I /usr/local/opt/binutils/include"
 # set -g -x LDFLAGS "$LDFLAGS -L /usr/local/opt/binutils/lib"
