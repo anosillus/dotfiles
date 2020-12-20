d09367627b3c2e3b384f8ded263a5f0e2a9dd972
anosillus <anosillus@gmail.com>
2020-12-21 03:32:26 +0900

    template fixed

--- dots/vim/template/template.py
+++ dots/vim/template/template.py
@@ -7,17 +7,17 @@
 Description
 """
 from dataclasses import dataclass
+import itertools
+from collections import Counter
 from typing import Callable
-from typing import Dict
 from typing import Final
-from typing import List
 from typing import NamedTuple
-from typing import Tuple
 from typing import TypedDict
 from typing import Union
 
 
 def main():
+    pass
     <+CURSOR+>
 
 
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     main()
 
 
-# vim: ft=python ts=4 sw=4 sts=4 tw=88 fenc='utf-8' ffs='unix' si et:
+# vim: ft=python ts=4 sw=4 sts=4 tw=88 fenc='utf-8' ff='unix' si et:
